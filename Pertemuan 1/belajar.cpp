3e8dc67c082e6dd6a8b4851ec9ea9bea98c94a8b
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-23 22:52:12 +0700

    Update belajar.cpp

--- Pertemuan 1/belajar.cpp
+++ Pertemuan 1/belajar.cpp
@@ -1,41 +1,40 @@
-
 #include <iostream>  // header untuk C++
 #include <conio.h> // header untuk fungsi getch() dan getche()
 using namespace std;
 
 int main() {
     
-    string nama;
-    char kom, jenisKelamin;
-    int nim;
-    float ip;
+    string nama; // variabel untuk menyimpan nama
+    char kom, jenisKelamin; // variabel untuk menyimpan KOM 
+    int nim; // variabel untuk menyimpan nim
+    float ip; // variabel untuk menympan IP
 
     /*ini untuk komen 
     beberapa baris*/
 
-    cout << "Hello World" << endl;
+    cout << "Hello World" << endl; // menampilkan pesan "Hello World"
 
-    cout << "Masukkan nama Anda : ";
-    // cin >> nama;
+    cout << "Masukkan nama Anda : "; // meminta pengguna untuk memasukkan nama
+    // cin >> nama; // diberi komentar agar tidak mengganggu input dengan spasi
     getline(cin, nama); // agar karakter spasi bisa terbaca
 
-    cout << "Masukkan KOM : ";
-    cin >> kom;
+    cout << "Masukkan KOM : "; // meminta pengguna untuk memasukkan KOM
+    cin >> kom; // membaca karakter untuk KOM
 
-    cout << "Masukkan NIM : ";
-    cin >> nim;
+    cout << "Masukkan NIM : "; // meminta pengguna untuk memasukkan NIM
+    cin >> nim; // membaca bilangan bulat untuk NIM
 
-    cout << "Masukkan IP : ";
-    cin >> ip;
+    cout << "Masukkan IP : "; // meminta pengguna untuk memasukkan IP
+    cin >> ip; // membaca bilangan desimal untuk IP
 
-    cout << "Masukkan jenis kelamin (L/P) :  ";
+    cout << "Masukkan jenis kelamin (L/P) :  "; // meminta pengguna nuntuk memasukkan jenis kelamin
     jenisKelamin = getche(); // agar karakter langsg tampil, tanpa tekan enter
 
     /*Untuk Output*/
-    cout << "\n" << nama << endl;
-    cout << nim << endl;
-    cout << kom << endl;
-    cout << ip << endl;
+    cout << "\n" << nama << endl; // menampilkan nama 
+    cout << nim << endl; // menampilkan NIM
+    cout << kom << endl; // menampilkan KOM
+    cout << ip << endl; // menamipilkan IP
     putchar(jenisKelamin); // untuk menampilkan karakter
 
     getch(); // karakter yang diketik ga di tampilkan di layar
