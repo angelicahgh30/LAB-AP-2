7934c2d42e1ccb340f9a83dc3391db66a1228585
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-23 23:39:46 +0700

    Update latihan.cpp

--- Pertemuan 2/latihan.cpp
+++ Pertemuan 2/latihan.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main()
     {
         // Program untuk menentukan volume dan luas sebuah bola
-        float luas, volume, jari;
-        const float phi = 3.14; // konstanta 
+        float luas, volume, jari; // variabel untuk menyimpan luas, volume dan jari-jari bola
+        const float phi = 3.14; // konstanta phi dengan nilai 3.14
 
-        cout << "Input jari-jari : ";
-        cin >> jari;
+        cout << "Input jari-jari : "; // meminta pengguna memasukkan jari-jari bola
+        cin >> jari; // membaca nilai jari-jari dari input pengguna
 
-        volume = ((float)4/3) * (phi) * (jari*jari*jari);
-        luas = 4 * phi * jari * jari;
+        volume = ((float)4/3) * (phi) * (jari*jari*jari); // rumus menghitung volume bola 
+        luas = 4 * phi * jari * jari; // rumus menghitung luas permukaan bola 
 
-        cout << "Volume : " << volume << endl;
-        cout << "luas : " << luas << endl;
+        cout << "Volume : " << volume << endl; // menampilkan hasil volume bola
+        cout << "luas : " << luas << endl; // menampilkan hasil luas permukaan bola
     }
