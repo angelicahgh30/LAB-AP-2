76073ad79458cf09c6e423a015e1f49d2aa27f28
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-24 22:56:47 +0700

    Operasi Kondisi

--- Pertemuan 3/main.cpp
+++ Pertemuan 3/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-long faktorial(int n){
-    long faktor;
+long faktorial(int n){ // fungsi rekursif untuk menghitung faktorial dari n
+    long faktor; // variabel untuk menyimpan hasil faktorial  
     if(n > 1){
-        faktor = n * faktorial(n-1);
-    }else if(n == 1 || n == 0){
+        faktor = n * faktorial(n-1); // jika n lebih dari 1, hitung faktorial secara rekursif
+    }else if(n == 1 || n == 0){ // basis rekursif
         faktor = 1;
     }else if(n < 0) {
         cout << "n tidak boleh negatif\n";
