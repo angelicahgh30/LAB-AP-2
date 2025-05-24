48a6f5a3781c051622943b3c543e93acb1d8d8fa
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-24 21:29:43 +0700

    Penggunaan Operator

--- Pertemuan 2/luasPpanjang.cpp
+++ Pertemuan 2/luasPpanjang.cpp
@@ -1,19 +1,18 @@
-
-#include <iostream>
+#include <iostream> // header untuk input dan output
 using namespace std;
 
 int main () {
 
-    float luas, p, l;
+    float luas, p, l; // variabel untuk menyimpan luas, panjang, dan lebar
 
-    cout << "Masukkan panjang : ";
-    cin >> p;
+    cout << "Masukkan panjang : "; // meminta pengguna untuk memasukkan panjang
+    cin >> p; // membaca nilai panjang dari input pengguna
 
-    cout << "Masukkan lebar : ";
-    cin >> l;
+    cout << "Masukkan lebar : "; // meminta pengguna untuk memasukkan lebar
+    cin >> l; // membaca nilai lebar dari input pengguna
 
-    luas = p * l;
-    cout << "Luas = " << luas << endl;
+    luas = p * l; // rumus untuk menghitung luas
+    cout << "Luas = " << luas << endl; // menampilkan hasil luas
 
     return 0;
 }
