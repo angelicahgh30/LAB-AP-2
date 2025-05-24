9f59403a7befeccf9e8eab9541d3461d4d683f29
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-24 21:43:16 +0700

    Operasi Kondisi

--- Pertemuan 3/latihan.cpp
+++ Pertemuan 3/latihan.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main() {
-    int angka;
-    cout << "Masukkan sebuah bilangan bulat: ";
-    cin >> angka;
+    int angka; // variabel untuk menhyimpan bilangan bulat yang dimasukkan
+    cout << "Masukkan sebuah bilangan bulat: "; // meminta pengguna memasukkan angka
+    cin >> angka; // membaca input angka
 
-    if (angka % 10 == 0) {
+    if (angka % 10 == 0) { // mengecek apakah angka merupakan kelipatan 10
         cout << angka << " adalah kelipatan dari 10." << endl;
-    } else if (angka % 5 == 0) {
+    } else if (angka % 5 == 0) { // jika bukan kelipatan 10, cek apakah kelipatan t5
         cout << angka << " adalah kelipatan dari 5." << endl;
-    } else {
+    } else { // jika bukan kelipatan 5 mauoun 10
         cout << angka << " bukan kelipatan dari 5 atau 10." << endl;
     }
 
