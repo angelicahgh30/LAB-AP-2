809194bbc2a89384d88262135567ed4fbcd22dad
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 16:21:44 +0700

    Perulangan

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main(){
-    string kalimat;
-    int i;
+    string kalimat; // deklarasi variabel bertipe string
+    int i; //  Variabel indeks untuk perulangan
 
     system("CLS");
 
-    cout << "Masukkan kalimat : ";
-    getline(cin, kalimat);
+    cout << "Masukkan kalimat : "; // Meminta pengguna memasukkan kalimat
+    getline(cin, kalimat); // Membaca kalimat dengan spasi
 
-    // a l y a 
+    // a n g e l
     for(i = 0; i < kalimat.length(); i++) {
-        kalimat[i] = toupper(kalimat[i]);
+        kalimat[i] = toupper(kalimat[i]); // Mengubah nilai variabel ke huruf kapital
     }
 
     cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
