3301b2b431cf087d7c3d9be2e9eb4f737cadf764
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 16:38:30 +0700

    Array & Vector

--- Pertemuan 5/array.cpp
+++ Pertemuan 5/array.cpp
@@ -3,120 +3,113 @@
 #include <array>
 using namespace std;
 
-int main () {
-    // Array Declaration & Initialization
-    /* 2 Cara Membuat Array */
+int main(){
+    string nama[5]; // deklarasi array 1 dimensi dengan lebar 5 elemen bertipe string
 
-    // cara 1 : array kosong
-    // string nama[5];
+    // mengisikan tiap indeks pada array 
+    nama[0] = "Angel";
+    nama[1] = "Nazwa";
+    nama[2] = "Nabilah";
+    nama[3] = "Nayla";
+    nama[4] = "Mutia";
 
-    // nama[0]="Alya";
-    // nama[1]="Parul";
-    // nama[2]="Syukron";
-    // nama[3]="Aurick";
-    // nama[4]="Dzakwan";
+    // Menampilkan secara Ascending
+    cout << nama[0] << endl;
+    cout << nama[1] << endl;
+    cout << nama[2] << endl;
+    cout << nama[3] << endl;
+    cout << nama[4] << endl;
 
-    // cara 2 : array yang langsung diisi
-    // string nama[5] = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"};
-    // string nama[] = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"};
+    // Menampilkan tiap elemen menggunakan for loop
 
-    // Ascending Element in Array
-    // cout << nama[0] << endl;
-    // cout << nama[1] << endl;
-    // cout << nama[2] << endl;
-    // cout << nama[3] << endl;
-    // cout << nama[4] << endl;
+    // secara Ascending
+    for(int i = 0;i < 5;i++){
+        cout << nama[i] << " ";
+    }
+    cout << endl;
 
-    // Menggunakan Looping
-    // for(int i = 0; i < 5; i++) {
-    //     cout << nama[i] << endl;
-    // }
+    // secara Descending
+    for(int i = 4;i >= 0;i--){
+        cout << nama[i] << " ";
+    }
+    cout << "\n\n";
 
-    // Descending element in array use looping
-    // for (int i = 4; i >= 0; i--) {
-    //     cout << nama[i] << endl;
-    // }
+    // Matriks Multidimensi
+    int matriks[2][3] = {{1,3,5},{2,4,6}}; // deklarasi array 2 dimensi
 
-
-
-    // Multidimensional Array
-    // 1 3 5
-    // 2 4 6
-
-    // int matrix[2][3] = {{1, 3, 5},{2, 4, 6}};
-    // for(int i = 0; i < 2; i++) {
-    //     for(int j = 0; j < 3; j++) {
-    //         cout << matrix[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    // int matrix[4][6] ={
-    //     {1, 2, 3, 4, 5, 6},
-    //     {7, 8, 9, 10, 11, 12},
-    //     {13, 14, 15, 16, 17, 18},
-    //     {19, 20, 21, 22, 23, 24}
-    // };
+    // menampilkan tiap elemen dengan nested for
+    for(int i = 0;i < 2;i++){
+        for(int j = 0;j < 3;j++){
+            cout << matriks[i][j] << " ";
+        }
+        cout << endl;
+    }
     
-    // for (int i = 0; i < 4; i++) { // 4 baris
-    //     for (int j = 0; j < 6; j++) { // 6 kolom
-    //         cout << matrix[i][j] << "\t"; // pakai \t biar rapi seperti tabel
-    //     }
-    //     cout << endl;
-    // }
+    // deklarasi array 2 dimensi dengan ukuran 4 baris dan 6 kolom
+    int matrix[4][6] = {{1,2,3,4,5,6},{7,8,9,10,11,12},{13,14,15,16,17,18},{19,20,21,22,23,24}};
+    for(int i = 0;i < 4;i++){
+        for(int j = 0;j < 6;j++){
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    string name = "Angel";
+    cout << name[0] << endl;
+    cout << name[2] << endl << endl;
 
+    for(int i = 0; i < name.length();i++){
+        cout << name[i] << " ";
+    }
 
-
-    // String (array of characters)
-    // string nama = "Kiel";
-    // cout << nama[0] << endl;
-    // cout << nama[2] << endl;
-
-    // for (int i = 0; i < nama.length(); i++) {
-    //     cout << nama[i] << endl;
-    // }
+    string s1 = "Hello";
+    string s2 = "World";
     
+    // s1 = s2 => melakukan re-assignment 
+    cout << endl << s1 << endl;
+    s1 = s2;
+    cout << s1 << endl;
+    
+    // s1 = s1 + s2 => menggabungkan 2 variabel bertipe string
+    s1 = "Hello";
+    s1 = s1 + " " + s2;
+    cout << s1 << endl;
+    
+    // s.length() => menghitung jumlah karakter dalam string
+    int panjang = s1.length(); // mengisikan jumlah karakter pada variabel 's1' ke dalam variabel 'panjang'
+    int panjang2 = s2.length();// mengisikan jumlah karakter pada variabel 's2' ke dalam variabel 'panjang2'
+    int jumlah_panjang = panjang+panjang2; // menjumlahkan banyak karakter keduanya
+    cout << panjang << endl;
+    
+    // substr(n, m) => Untuk mengambil sebagian string, yang dimana n indeks nya, m merupakan banyak karakter yang mau diambil
+    s1 = "Hello";
+    s2 = "World";
+    string jumlah_str = s1 + s2;
+    cout << s1.substr(2,4) << endl; // dimulai dari indeks ke-2, sebanyak 4 karakter
+    cout << s2.substr(1,3) << endl; // dimulai dari indeks ke-1, sebanyak 3 karakter
+    cout << jumlah_str << endl;
+    cout << jumlah_str.substr(4,2) << endl; // dimulai dari indeks ke-4, sebanyak 2 karakter
 
-    // string s1 = "Hello"; 
-    // string s2 = "World"; 
+    // sizeof => untuk mengetahui ukuran dari variabel, array, dll
+    int angka = 10;
+    cout << endl << sizeof(angka); // Mengembalikan ukuran byte dari tipe data variabelnya;
 
-    /* 1. s1 = s2 */
-    // s1 = s2;
-    // cout << "s1 = " << s1 << endl;
+    int nilai[5] = {10,20,30,40,50};
+    cout << sizeof(nilai[5]);
 
-    /* 2. s1 = s1 + s2 */
-    // s1 = "Hello";  // reset nilai di s1
-    // cout << "Hasil : " << s1 + s2 << endl;
+    string s = "Angel";
+    cout << endl << sizeof(s) << endl;
 
-    /* 3. s1. length() */
-    // cout << s1.length() << endl;
-    // cout << (s1 + s2).length() << endl;
+    // Array Library
+    array<float, 5> nilai_array = {87.5,98,100,95.7,78.3}; // deklarasi array bertipe float dengan banyak 5 elemen
+    cout << "Nilai : ";
+    for(int i = 0;i < nilai_array.size(); i++){
+        cout << nilai_array[i] << " ";
+    }
+    cout << endl;
 
-    /* 4. s1.substr(n, m) */
-    // cout << s1.substr(2, 3) << endl;
-    // cout << (s1 + s2).substr(5, 3) << endl;
-
-
-    // Operator sizeof
-    // int angka = 10; 
-    // cout << sizeof(angka);
-
-    // int nilai[] = {10, 20, 30, 40, 50};
-    // cout << sizeof(nilai);
-
-    // string s = "Alya";
-    // cout << sizeof(s);
-
-
-    // Array library
-    // array<float, 5> nilai = {87.5, 90, 100, 95.7, 78.3};
-    // cout << "Nilai : ";
-
-    // for(int i = 0; i < nilai.size(); i++) {
-    //     cout << nilai[i] << " ";
-    // }
-
-    // for (float n : nilai) {
-    //     cout << n << " ";
-    // }
+    // range-based for loop
+    for(float n : nilai_array){ // setiap elemen dari 'nilai_array' akan disimpan dalam variabel n
+        cout << n << " ";
+    }
 }
