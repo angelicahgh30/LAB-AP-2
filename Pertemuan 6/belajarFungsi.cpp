f95f4134ab0809d520d8289db196b5b4c38daf75
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 17:55:36 +0700

    Fungsi & Header

--- Pertemuan 6/belajarFungsi.cpp
+++ Pertemuan 6/belajarFungsi.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// Fungsi sapa yang menerima parameter string nama dan menampilkan pesam sapaan kepengguna
 void sapa(string nama) {
     cout << "Halo " << nama << " ! Selamat Datang di AP 2 !" << endl;
 }
 
 int main () {
-    string namaPengguna = "Angel";
+    string namaPengguna = "Angel"; // variabel nama pengguna
 
-    sapa(namaPengguna);
+    sapa(namaPengguna); // memanggil fungsi sapa dengan argumen nama pengguna
 
     return 0;
 }
