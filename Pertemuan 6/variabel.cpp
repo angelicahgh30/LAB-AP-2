6c14659337011febd4d398dcdd063e340037972c
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 18:10:21 +0700

    Fungsi & Header

--- Pertemuan 6/variabel.cpp
+++ Pertemuan 6/variabel.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
 
-string namaGlobal = "Ilmu";
-
+string namaGlobal = "Ilmu"; // Variabel global => dapat diseluruh program
 void namaVariabel() {
-    string namaLokal = "Komputer";
+    string namaLokal = "Komputer";  // variabel lokal => hanya bisa diakses di dalam fungsi
 
-    // coba akses
+
+    // coba akses variabel lokal
     cout << namaLokal << endl;
 
-    // coba akses
+    // coba akses variabel global
     cout << namaGlobal << endl;
 }
 
 int main () {
-    namaVariabel();
+    namaVariabel(); // memanggil fungsi namaVariabel
 
-    // coba akses
+    // coba akses variabel global dari main
     cout << namaGlobal << endl;
 
-    // coba akses
-    // cout << namaLokal << endl; // ga iso
+    // coba akses variabel lokal dari main (tidak bisa karena hanya ada di fungsi namaVariabel)
+    // cout << namaLokal << endl; // ga iso, akan error jika diuncomment
 
 }
