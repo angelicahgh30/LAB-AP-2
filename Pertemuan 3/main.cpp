6ec1ec7e12df88d918f7770ae2d198d0e5859725
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 16:03:31 +0700

    Operasi Kondisi

--- Pertemuan 3/main.cpp
+++ Pertemuan 3/main.cpp
@@ -5,20 +5,20 @@ long faktorial(int n){ // fungsi rekursif untuk menghitung faktorial dari n
     long faktor; // variabel untuk menyimpan hasil faktorial  
     if(n > 1){
         faktor = n * faktorial(n-1); // jika n lebih dari 1, hitung faktorial secara rekursif
-    }else if(n == 1 || n == 0){ // basis rekursif
+    }else if(n == 1 || n == 0){ // basis rekursif => faktorial dari 0 dan 1
         faktor = 1;
-    }else if(n < 0) {
+    }else if(n < 0) { // utnuk input negatif faktorial tidak terdefinisi
         cout << "n tidak boleh negatif\n";
-        faktor = 0;
+        faktor = 0; // // Mengembalikan 0 untuk input negatif
     }
     return faktor;
 }
 
 int main(){
-    long nilai,hasil;
-    cout << "Masukkan Nilai n : ";cin >> nilai;
-    hasil = faktorial(nilai);
-    cout << nilai << "!  = " << hasil << endl;
+    long nilai,hasil; // Variabel untuk menyimpan input dan hasil
+    cout << "Masukkan Nilai n : ";cin >> nilai; // Meminta pengguna memasukkan nilai n
+    hasil = faktorial(nilai); // Memanggil fungsi faktorial
+    cout << nilai << "!  = " << hasil << endl; // Menampilkan hasil faktorial
 
     return 0;
 }
