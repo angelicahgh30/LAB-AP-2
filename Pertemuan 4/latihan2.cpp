1822c5f2f28449f5a54022b386f117d9f090a9ae
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 16:24:20 +0700

    Perulangan

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -5,17 +5,17 @@ int main() {
     string kalimat;
     
     cout << "Masukkan sebuah kalimat: ";
-    getline(cin, kalimat);
+    getline(cin, kalimat); // Membaca kalimat dengan spasi
 
     for (int i = 0; i < kalimat.length(); i++) {
         // Cek apakah karakter adalah huruf vokal
         if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || 
             kalimat[i] == 'o' || kalimat[i] == 'u') {
-            kalimat[i] = toupper(kalimat[i]); // Ubah ke huruf kapital
+            kalimat[i] = toupper(kalimat[i]); // Ubah ke huruf vokal menjadi kapital
         }
     }
 
-    cout << "Hasil: " << kalimat << endl;
+    cout << "Hasil: " << kalimat << endl; // Menampilkan hasil kalimat setelah mengubah huruf vokal menjadi kapital
 
     return 0;
 }
