6341d762f69c8f70576b9209a670f14f95f80cbf
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 16:50:39 +0700

    Array & Vector

--- Pertemuan 5/vector.cpp
+++ Pertemuan 5/vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <vector>
+#include <vector> // header vector untuk bisa menggunakan vector
+#include <string> // berisi fungsi fungsi untuk operasi string
 
 using namespace std;
 
@@ -8,28 +9,28 @@ int main (){
     vector<string> nama_kayawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"};
 
     // Ascending Element of Vector
-    // for(int i = 0; i < nama_kayawan.size(); i++) {
-    //     cout << nama_kayawan[i] << endl;
-    // }
+    for(int i = 0; i < nama_kayawan.size(); i++) {
+        cout << nama_kayawan[i] << endl; // menampilkan elemen vector satu per satu berdasarkan indeks
+    }
 
-    // for (string karyawan : nama_kayawan) {
-    //     cout << karyawan << endl;
-    // }
+    for (string karyawan : nama_kayawan) {
+        cout << karyawan << endl; // menampilkan elemen vector menggunakan range-based for loop
+    }
 
 
     // Add data to Vector
     nama_kayawan.push_back("Imam");
 
-    // for(int i = 0; i < nama_kayawan.size(); i++) {
-    //     cout << nama_kayawan[i] << endl;
-    // }
+    for(int i = 0; i < nama_kayawan.size(); i++) {
+        cout << nama_kayawan[i] << endl;  // menampilkan vector setelah penambahan elemen baru
+    }
 
     
     // Delete data from Vector
-    nama_kayawan.pop_back();
-    nama_kayawan.erase(nama_kayawan.begin() + 3);
+    nama_kayawan.pop_back(); // menghapus elemen terakhir dari vector
+    nama_kayawan.erase(nama_kayawan.begin() + 3); // menghapus elemen pada indeks ke-3 (elemen keempat)
 
     for(int i = 0; i < nama_kayawan.size(); i++) {
-        cout << nama_kayawan[i] << endl;
+        cout << nama_kayawan[i] << endl; // menampilkan vector setelah penghapusan elemen
     }
 }
