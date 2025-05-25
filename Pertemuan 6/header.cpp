543f5f15dfeee5df98dd9ccdbbda5f10fafc22aa
Angelica H.G Harianja <angelicaharianja30@gmail.com>
2025-05-25 18:06:02 +0700

    Fungsi & Header

--- Pertemuan 6/header.cpp
+++ Pertemuan 6/header.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
-#include <cmath>
+#include <iostream> // Header => berisi fungsi fungsi yang dibuat dari bahasa pemrograman
+#include <cmath> // header berisi operasi matematika yg lebih kompleks
 
 using namespace std;
 
+// Fungsi untuk menjumlahkan dua bilangan bulat
 int jumlah (int a, int b) {
     return a + b;
 }
 
+// Fungsi untuk mengurangkan dua bilangan bulat
 int kurang (int a, int b) {
     return a - b;
 }
@@ -15,10 +17,12 @@ int main (){
     system ("CLS");
 
     int n;
-    cout << jumlah (2,3)  << endl;
-    cout << kurang(2,3)  << endl;
+    cout << jumlah (2,3)  << endl; // Menampilkan hasil penjumlahan
+    cout << kurang(2,3)  << endl; // Menampilkan hasil pengurangan
 
-    double angka = 25.0 ;
-    double akar = sqrt (angka);
+    double angka = 25.0 ; // Variabel untuk menyimpan angka
+    double akar = sqrt (angka); // // Menghitung akar kuadrat dari angka
+    
+    // Menampilkan hasil akar kuadrat
     cout << "Akar kuadrat dari  " << angka << "adalah : " << akar << endl;
 }
